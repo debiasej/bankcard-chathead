60b4d0bae29463f2be1a0ad691e548e0400c7db3
Mario de Biase <debiasej@gmail.com>
2017-01-20 10:44:29 +0100

    Create bankcard service

--- app/src/main/java/com/debiasej/bankcard/BankcardService.java
+++ app/src/main/java/com/debiasej/bankcard/BankcardService.java
@@ -4,9 +4,13 @@ package com.debiasej.bankcard;
  * Created by Mario de Biase on 19/1/17.
  */
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v7.app.NotificationCompat;
 import android.view.WindowManager;
 
 
@@ -28,13 +32,37 @@ public class BankcardService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
+        if (intent.getBooleanExtra("stop_service", false)){
+            // If it's a call from the notification tray, stop the service.
+            stopSelf();
+        }else{
+            // Make the service run in foreground so that the system does not shut it down.
+            NotificationManager mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            Intent notificationIntent = new Intent(this, BankcardService.class);
+            notificationIntent.putExtra("stop_service", true);
+            PendingIntent pendingIntent = PendingIntent.getService(this, 0, notificationIntent, 0);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                    this);
+            Notification notification = builder.setContentIntent(pendingIntent)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setTicker("Bankcard Chathead launched")
+                    .setWhen(System.currentTimeMillis())
+                    .setAutoCancel(true)
+                    .setContentTitle("Bankcard Chathead")
+                    .setContentText("Tap to remove the Bankcard Chathead").build();
+            mNM.notify(86, notification);
+        }
+
         return Service.START_STICKY;
     }
 
+    @Override
+    public void onDestroy() {
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // Not used
         return null;
     }
 }
--- app/src/main/java/com/debiasej/bankcard/MainActivity.java
+++ app/src/main/java/com/debiasej/bankcard/MainActivity.java
@@ -1,13 +1,39 @@
 package com.debiasej.bankcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final Class SERVICE_CLASS = BankcardService.class;
+    private Button startService,stopService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startService=(Button)findViewById(R.id.startService);
+        startService.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(getApplication(), SERVICE_CLASS));
+            }
+        });
+
+        stopService=(Button)findViewById(R.id.stopService);
+        stopService.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(getApplication(), SERVICE_CLASS));
+            }
+        });
+
     }
 }
