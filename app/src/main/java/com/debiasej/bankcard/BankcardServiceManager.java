808df6145f7a1aa474cd8d1939f8d6cd3fdcaf39
Mario de Biase <debiasej@gmail.com>
2017-01-20 09:21:17 +0100

    Create presenter and facade of the presentation layer

---
+++ app/src/main/java/com/debiasej/bankcard/BankcardServiceManager.java
@@ -0,0 +1,10 @@
+package com.debiasej.bankcard;
+
+/**
+ * Created by Mario de Biase on 20/1/17.
+ */
+
+public interface BankcardServiceManager {
+
+
+}
---
+++ app/src/main/java/com/debiasej/bankcard/BankcardServicePresenter.java
@@ -0,0 +1,15 @@
+package com.debiasej.bankcard;
+
+/**
+ * Created by Mario de Biase on 20/1/17.
+ */
+
+public class BankcardServicePresenter {
+
+    private final BankcardServiceManager serviceManager;
+
+    public BankcardServicePresenter(BankcardServiceManager serviceManager) {
+        this.serviceManager = serviceManager;
+    }
+
+}
